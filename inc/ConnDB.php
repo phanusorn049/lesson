bbaaac883ff7e2e581698f1817041c27c49dd036
phanusorn049 <phanusorn.p@psru.ac.th>
2026-09-21 10:37:34 +0700

    Add files via upload

--- inc/ConnDB.php
+++ inc/ConnDB.php
@@ -1,1 +1,21 @@
+<?php
+//edit Here for Config Connection Database
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "db_northwind";
 
+try {
+       $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+       $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+   } catch(PDOException $e) {
+       http_response_code(500);
+       echo json_encode([
+           'success' => false,
+           'message' => 'เกิดข้อผิดพลาดในการเชื่อมต่อฐานข้อมูล: ' . $e->getMessage()
+       ], JSON_UNESCAPED_UNICODE);
+       exit;
+   }
+
+
+?>
