6823a3c140e61b1e4d7b2967bc3fb481746abc11
phanusorn049 <phanusorn.p@psru.ac.th>
2026-09-23 15:09:02 +0700

    Update ConnDB.php

--- inc/ConnDB.php
+++ inc/ConnDB.php
@@ -16,7 +16,7 @@ try {
         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_EMULATE_PREPARES   => false,
-        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4"
+        // ตัดบรรทัด PDO::MYSQL_ATTR_INIT_COMMAND ออกเพื่อแก้ปัญหา Undefined Constant
     ];
 
     $conn = new PDO($dsn, $user, $password, $options);
