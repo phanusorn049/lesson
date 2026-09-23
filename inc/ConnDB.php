ba43e304a6ea98710e6f17ad6b55f8f1a19f2680
phanusorn049 <phanusorn.p@psru.ac.th>
2026-09-23 10:03:54 +0700

    Update ConnDB.php

--- inc/ConnDB.php
+++ inc/ConnDB.php
@@ -1,21 +1,19 @@
 <?php
-//edit Here for Config Connection Database
-$servername = "localhost";
-$username = "root";
-$password = "root";
-$dbname = "db_northwind";
+$servername = getenv('MYSQLHOST');
+$port       = getenv('MYSQLPORT');
+$username   = getenv('MYSQLUSER');
+$password   = getenv('MYSQLPASSWORD');
+$dbname     = getenv('MYSQLDATABASE');
 
 try {
-       $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-       $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-   } catch(PDOException $e) {
-       http_response_code(500);
-       echo json_encode([
-           'success' => false,
-           'message' => 'เกิดข้อผิดพลาดในการเชื่อมต่อฐานข้อมูล: ' . $e->getMessage()
-       ], JSON_UNESCAPED_UNICODE);
-       exit;
-   }
-
-
+    $conn = new PDO("mysql:host=$servername;port=$port;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    http_response_code(500);
+    echo json_encode([
+        'success' => false,
+        'message' => 'เกิดข้อผิดพลาดในการเชื่อมต่อฐานข้อมูล: ' . $e->getMessage()
+    ], JSON_UNESCAPED_UNICODE);
+    exit;
+}
 ?>
